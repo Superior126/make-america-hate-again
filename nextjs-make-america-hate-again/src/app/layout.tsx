15861ce61caeaf6d6ecf68062d50366282cdefe4
Superior125 <90008321+Superior126@users.noreply.github.com>
2025-03-14 00:02:04 -0400

    Add MS Clarity support

--- nextjs-make-america-hate-again/src/app/layout.tsx
+++ nextjs-make-america-hate-again/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import MS_Clarity from "./components/ms_clarity";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <MS_Clarity />
         {children}
       </body>
     </html>
