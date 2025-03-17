59d182717cd4d6f168dd89418480b6f1443ce985
Superior125 <90008321+Superior126@users.noreply.github.com>
2025-03-17 15:24:54 -0400

    remove header image

--- nextjs-make-america-hate-again/src/app/page.tsx
+++ nextjs-make-america-hate-again/src/app/page.tsx
@@ -19,13 +19,6 @@ export default async function IndexPage() {
   return (
     <main className="container mx-auto min-h-screen max-w-3xl p-8">
       <header>
-        <div className="header-image-container">
-          <img className="header-image" src="https://www.commondreams.org/sites/default/files/views-article/make_america_hate_again.jpg" alt="header image" />
-          <a className="header-image-source" href="https://www.commondreams.org/views/2017/08/15/making-america-hate-again" target="_blank">
-            <img src="/link.svg" alt="" />
-            <p>Image Source</p>
-          </a>
-        </div>
         <h1 className="header-title">Make America Hate Again</h1>
         <p className="header-description">A collection of things that republicans are doing to destroy America.</p>
       </header>
